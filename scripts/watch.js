8bc266592689b39d37b9f23fd1727c079d6bdcf8
Chase Adams <hey@chaseadams.io>
2018-12-19 22:26:57 -0800

    watch: update npm run to yarn

--- scripts/watch.js
+++ scripts/watch.js
@@ -2,7 +2,7 @@ const chokidar = require('chokidar');
 const spawn    = require('lighter-spawn');
 
 chokidar.watch('src', {ignored: /(^|[\/\\])\../}).on('change', () => {
-  spawn('npm run build')
+  spawn('yarn build')
     .on('out', (files) => { console.log(files) })
     .on('close',    () => { console.log('Done') })
     .on('error', (err) => { console.log(err); });
